e9f4bfc4123652b1a3d3607aa93280fd68965ded
cailqk <tsvetan.bogdanov@abv.bg>
2022-02-21 19:13:04 +0200

    Added score display / Made score display and play screen responsive when start is pressed

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 const startScreen = document.querySelector('.start-screen');
 const playScreen = document.querySelector('.play-screen');
+const gameScore = document.querySelector('.game-score')
 
 startScreen.addEventListener('click', (e) => {
     e.currentTarget.classList.add('hidden');
     playScreen.classList.remove('hidden');
+    gameScore.classList.remove('hidden');
+
 })
 
