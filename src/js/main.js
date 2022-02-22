40dfccf9dbefc06680a3b03fa09dc1f681202d53
cailqk <tsvetan.bogdanov@abv.bg>
2022-02-22 12:04:55 +0200

    Created function to track the push and release of buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+//creating elements
 const startScreen = document.querySelector('.start-screen');
 const playScreen = document.querySelector('.play-screen');
 const gameScore = document.querySelector('.game-score');
@@ -7,12 +8,28 @@ wizard.classList.add('wizard');
 wizard.style.top = '200px';
 wizard.style.left = '200px';
 
+let keys = {};
 
+//key listeners for movement of the wizard 
+document.addEventListener('keydown', onKeyDown, false);
+document.addEventListener('keyup', onKeyUp, false);
 startScreen.addEventListener('click', (e) => {
     e.currentTarget.classList.add('hidden');
     playScreen.classList.remove('hidden');
     gameScore.classList.remove('hidden');
-
 })
+
+//setting function to 
+function onKeyDown(e) {
+    keys[e.code] = true;
+    console.log(keys);
+}
+function onKeyUp(e) {
+    keys[e.code] = false;
+    console.log(keys);
+}
+
+
+
 
 playScreen.appendChild(wizard);
