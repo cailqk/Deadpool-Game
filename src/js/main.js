b1dc1e74c6587c54830956e22e9cb1231570658a
cailqk <tsvetan.bogdanov@abv.bg>
2022-02-22 11:30:06 +0200

    Created wizard element and added it to the play screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 const startScreen = document.querySelector('.start-screen');
 const playScreen = document.querySelector('.play-screen');
-const gameScore = document.querySelector('.game-score')
+const gameScore = document.querySelector('.game-score');
+const wizard = document.createElement('div');
+
+wizard.classList.add('wizard');
+wizard.style.top = '200px';
+wizard.style.left = '200px';
+
 
 startScreen.addEventListener('click', (e) => {
     e.currentTarget.classList.add('hidden');
@@ -9,3 +15,4 @@ startScreen.addEventListener('click', (e) => {
 
 })
 
+playScreen.appendChild(wizard);
